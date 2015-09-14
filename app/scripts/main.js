77ca7b44073f5210a93d96cc4c4e844785d334cc
Jaime Garcia <jaimegarcia@ingeniosolido.com>
2015-09-14 00:41:21 -0500

    Base structure for vega js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
 console.log('\'Allo \'Allo!');
+function parse(spec) {
+  console.log(spec);
+  vg.parse.spec(spec, function(chart) {
+    console.log(chart)
+    var view = chart({ el:"#vis" });
+    view.viewport(null)
+      .renderer("svg")
+      .update();
+  });
+}
+parse("charts-specs/bar-chart.json");
